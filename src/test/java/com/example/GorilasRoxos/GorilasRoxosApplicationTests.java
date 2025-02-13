df18a09f6d8347bf06a54a68a7f564c9df9c42a1
GuiSenac <guilherme.ltavares@senacsp.edu.br>
2025-02-13 16:55:23 -0300

    Initial Commit

---
+++ src/test/java/com/example/GorilasRoxos/GorilasRoxosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GorilasRoxos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorilasRoxosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
